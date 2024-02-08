39122b2577d40b73d0ac6d1e13a91bf2567c4367
Dennis Koob <d.koob@fh-aachen.de>
2024-02-08 14:48:15 +0100

    added own preprocessor information to get the profile information on every site

---
+++ habit_tracker/habits/context_processors.py
@@ -0,0 +1,18 @@
+from .models import ExperiencePoint, Level
+from django.db.models import Sum
+
+def profile_info(request):
+    user = request.user
+    total_xp = ExperiencePoint.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
+    user_level = calculate_user_level(total_xp)
+    return {'user_level': user_level, 'total_xp': total_xp}
+
+def calculate_user_level(total_xp):
+    levels = Level.objects.all().order_by('level_number')
+    user_level = 1
+    for level in levels:
+        if total_xp >= level.xp_threshold:
+            user_level = level.level_number
+        else:
+            break
+    return user_level
