49939f17283945a806084e1264604ad687ee8d6f
Koob <dennis.koob@alumni.fh-aachen.de>
2024-02-10 01:10:53 +0100

    fixed context processor to handle not logged in navbar user informations

--- habit_tracker/habits/context_processors.py
+++ habit_tracker/habits/context_processors.py
@@ -2,10 +2,13 @@ from .models import ExperiencePoint, Level
 from django.db.models import Sum
 
 def profile_info(request):
-    user = request.user
-    total_xp = ExperiencePoint.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
-    user_level = calculate_user_level(total_xp)
-    return {'user_level': user_level, 'total_xp': total_xp}
+    if(request.user.id):
+        user = request.user
+        total_xp = ExperiencePoint.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum'] or 0
+        user_level = calculate_user_level(total_xp)
+        return {'user_level': user_level, 'total_xp': total_xp}
+    else:
+        return {}
 
 def calculate_user_level(total_xp):
     levels = Level.objects.all().order_by('level_number')
