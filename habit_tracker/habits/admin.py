28c436b1b777abf8e2017f4316dba11bc09b8fe0
Koob <dennis.koob@alumni.fh-aachen.de>
2024-02-11 20:33:57 +0100

    updated adminpanel

--- habit_tracker/habits/admin.py
+++ habit_tracker/habits/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HabitCompletion, Habit, Egg, Bird, UserEggs, UserBirds
+from .models import HabitCompletion, Habit, Egg, Bird, UserEggs, UserBirds, Food
 
 # Register your models here.
 admin.site.register(HabitCompletion)
@@ -8,3 +8,4 @@ admin.site.register(Egg)
 admin.site.register(Bird)
 admin.site.register(UserEggs)
 admin.site.register(UserBirds)
+admin.site.register(Food)
