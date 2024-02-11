7ca6cd51919cf596e6942e80a7e5c9355026ee0e
Koob <dennis.koob@alumni.fh-aachen.de>
2024-02-11 15:43:04 +0100

    fixed static things

--- habit_tracker/habit_tracker/settings.py
+++ habit_tracker/habit_tracker/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
