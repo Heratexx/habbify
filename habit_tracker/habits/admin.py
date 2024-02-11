2d870b5d30f1a2649c80f8f63ba29ecea3828200
Koob <dennis.koob@alumni.fh-aachen.de>
2024-02-11 20:03:44 +0100

    adde models to admin backend

--- habit_tracker/habits/admin.py
+++ habit_tracker/habits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import HabitCompletion, Habit, Egg, Bird, UserEggs, UserBirds
 
 # Register your models here.
+admin.site.register(HabitCompletion)
+admin.site.register(Habit)
+admin.site.register(Egg)
+admin.site.register(Bird)
+admin.site.register(UserEggs)
+admin.site.register(UserBirds)
